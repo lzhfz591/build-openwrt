0abec7e5ed26803d87709f4a48fbf2065b30cdbb
lzhfz591 <110750615+lzhfz591@users.noreply.github.com>
2023-10-14 15:35:17 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,8 +8,9 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-src-git telephony https://git.openwrt.org/feed/telephony.git
-src-git small https://github.com/kenzok8/small-package
+sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
+
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
